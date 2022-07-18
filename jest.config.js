11f7dc0675886c061ce602dc18dd475d7cc4cdf5
Lucas <hairmare@purplehaze.ch>
2022-07-18 21:33:07 +0200

    chore(deps): update em all

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,13 @@ module.exports = {
   preset: "ts-jest",
   verbose: true,
   testEnvironment: "jsdom",
-  resolver: 'jest-node-exports-resolver',
   moduleFileExtensions: ["js", "ts", "vue"],
   transform: {
     "^.+\\.vue$": "@vue/vue3-jest",
     "^.+\\js$": "babel-jest",
   },
   transformIgnorePatterns: ["/node_modules/(?!@directus/shared)"],
+  testEnvironmentOptions: {
+    customExportConditions: ["node", "node-addons"],
+  }
 };
